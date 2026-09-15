c2975d70e8584665b935a5c57fdaa4eb1e5884c5
DAVI GABRIEL DOS SANTOS <davi_g_dos-santos@estudante.sesisenai.org.br>
2026-09-14 22:26:44 -0300

    minha alteração

---
+++ exemplo07-2.js
@@ -0,0 +1,12 @@
+let nota = 85;
+
+switch (dia) {
+  case nota >= 90:
+    console.log('Conceito A');
+    break;
+  case nota >= 80:
+    console.log('Conceito B');
+    break;
+  default:
+    console.log("Melhore!.");
+}
